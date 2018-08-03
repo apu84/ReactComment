21aa2f7300741eaaab222531b04f82235a2db4f4
Md. Moshlehuddin Mazumder <apu@escenic.com>
2018-08-03 16:23:36 +0600

    Removed unused user reducer

--- src/app/reducers/index.jsx
+++ src/app/reducers/index.jsx
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux';
 import comments from './comments';
-import users from './users';
 import loggedInUser from './loggedInUser'
 import notifications from './notifications'
 
 const allReducers = combineReducers({
   comments: comments,
-  users: users,
   loggedInUser: loggedInUser,
   notifications: notifications
 });
